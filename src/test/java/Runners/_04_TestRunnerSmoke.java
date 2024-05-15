40cdd4624cfac6ec638d0cc2859f30b261dd892f
umiteser <eser_umit@hotmail.com>
2024-05-15 13:42:43 +0300

    Jenkins report eklendi

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         tags = "@SmokeTest", // bütün senaryolarda SmokeTest tag ine sahip olanları çalıştır
         features = {"src/test/java/FeatureFiles"}, //klasördeki bütün feature lar
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        plugin = {"json:target/site/cucumber.json"} //jenkinste report olusması icin JSON formatinda sonucu almasi lazim
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
